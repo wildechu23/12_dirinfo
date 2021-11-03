c1f1bf9e3292afe8968f44b5f5fae25196cb5737
Wilde Chu <wildechu23@gmail.com>
2021-11-03 18:27:04 -0400

    update

--- dirinfo.c
+++ dirinfo.c
@@ -3,12 +3,20 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <errno.h>
+#include <string.h>
 
 
-int main() {
+int main(int argc, char *argv[]) {
+
+
     DIR *dir;
-    char *path = "./";
-    dir = opendir(path);
+    char *path;
+    if(argc == 2) path = argv[1];
+    if((dir = opendir(path)) == NULL) {
+        printf("Error: %s\n", strerror(errno));
+        return 0;
+    }
     struct dirent *entry;
 
     printf("Statistics of directory: %s\n", path);
